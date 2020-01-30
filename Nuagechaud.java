79418542561d7803ed4db6c48760337631d18a85
petitdeveloppeur <valentindylan@hotmail.fr>
2020-01-30 15:02:34 +0100

    chaud

---
+++ Nuagechaud.java
@@ -0,0 +1,25 @@
+package com.Nouveau.nuage;
+
+public class Nuagechaud extends Nuage {
+
+
+
+public void mouvement() {
+	
+	
+		System.out.println("Voyons..");
+	
+	
+	if ((Temperature > 25) && (humidite > 19))
+	{
+	System.out.println("Il y a un risque d'orage...");
+	}
+	else 
+	{
+		System.out.println("Il fait pas tous les jours un temps pareil !");
+	}
+
+	System.out.println("Hello World !");
+}} 	
+
+	
